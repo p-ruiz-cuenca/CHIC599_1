8c98fd9276bddf27d02b6bd25de63d40148374c4
Ruiz Cuenca <ruizcuen@lancaster.ac.uk>
2022-02-24 17:35:51 +0000

    Introduced data sources and more data cleaning

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# CHIC599_1
-Research Project 1 (aka mini-project)
+# _Research Project 1 (aka mini-project)_
+
+## Data sources 
+
+Temperature: Met Office Hadley Centre Central England Temperature Data (https://www.metoffice.gov.uk/hadobs/hadcet/data/download.html)
+
+Rainfall: Met Office Hadley Centre observations datasets: HadUKP (https://www.metoffice.gov.uk/hadobs/hadukp/)
+
+COVID: gov.uk - UK Coronavirus Dashboard (https://coronavirus.data.gov.uk/)
+
+Population: ONS Population estimates (https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/populationestimatesforukenglandandwalesscotlandandnorthernireland)
--- data_cleaning.R
+++ data_cleaning.R
@@ -1,6 +1,7 @@
 # 1.Load data ====
+library(reshape2)
 
-## Temperature data ------
+## Mean Temperature data ------
 
 temp <- read.fwf("raw_data/daily_HadCET_1772_2022.txt",
                  widths = c(6, rep(5, 13)),
@@ -14,6 +15,24 @@ names(temp) <- c("Year", "Day", "Jan", "Feb", "Mar", "Apr",
 # change -999 to NA
 temp[temp == -999] <- NA
 
+# wide to long
+temp <- melt(temp, id = c("Year", "Day"))
+names(temp)[c(3,4)] <- c("Month", "mean.temp")
+
+# change month to factor
+
+temp$Month <- factor(temp$Month,
+                     levels = c("Jan", "Feb", "Mar", "Apr",
+                                "May", "Jun", "Jul", "Aug",
+                                "Sep", "Oct", "Nov", "Dec"),
+                     ordered = TRUE)
+
+# order data frame by Yeah, Month and then Day 
+
+temp <- temp[order(temp$Year,
+                   temp$Month,
+                   temp$Day),]
+
 ## Rainfall data -----
 
 rain <- read.fwf("raw_data/daily_rainfall_NWEP_HadUKP.txt", 
@@ -26,3 +45,15 @@ names(rain) <- c("Year", "Month", seq(1, 31, by = 1))
 # change -99.99 to NA
 
 rain[rain==-99.99] <- NA
+
+## COVID data ----
+
+covid <- read.csv("raw_data/region_2022-02-07.csv")
+
+## Population data ----
+# copied from "raw_data/ukpopestimatesmid2020on2021geography.xls"
+  # sheet "MYE2 - Persons"
+
+# population estimate for North-West England for mid-2020
+pop <- 7367456
+
