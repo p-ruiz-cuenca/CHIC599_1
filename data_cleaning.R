560d8368a6f7c987f45a822c420914d052b1f94e
Ruiz Cuenca <ruizcuen@lancaster.ac.uk>
2022-02-25 15:32:13 +0000

    new covid data (24/02/22, fixes issue with NewCasesPublishDate, uses NewCasesSpecimenDate instead), final data cleaning and creation of covid_data.csv

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,8 +1,10 @@
 # 1.Load data ====
 library(reshape2)
 library(stringr)
+library(lubridate)
 
-## Temperature data ------
+## a. Temperature data ------
+# units = degrees C
 
 # Create function to import temperature data
 
@@ -55,7 +57,7 @@ clean.temp.data <- function(data, temp.name){
     
   }
   
-  # order data frame by Yeah, Month and then Day 
+  # order data frame by Year, Month and then Day 
   
   temp <- temp[order(temp$Year,
                      temp$Month,
@@ -75,6 +77,23 @@ min.temp <- clean.temp.data(data = "raw_data/daily_min_HadCET_1878_2022.txt",
 max.temp <- clean.temp.data(data = "raw_data/daily_max_HadCET_1878_2022.txt",
                             temp.name = "max.temp")
 
+mean.temp$date <- paste(mean.temp$Year, as.numeric(mean.temp$Month), mean.temp$Day,
+                        sep = "-")
+
+mean.temp$date <- ymd(mean.temp$date)
+
+min.temp$date <- paste(min.temp$Year, as.numeric(min.temp$Month), min.temp$Day,
+                        sep = "-")
+
+min.temp$date <- ymd(min.temp$date)
+
+max.temp$date <- paste(max.temp$Year, as.numeric(max.temp$Month), max.temp$Day,
+                        sep = "-")
+
+max.temp$date <- ymd(max.temp$date)
+
+
+
 # plot data
 
 plot(1:length(mean.temp$mean.temp[mean.temp$Year > 2019]),
@@ -91,7 +110,8 @@ matplot(1:length(mean.temp$mean.temp[mean.temp$Year > 2020]),
         xlab = "Day",
         ylab = "Temp")
 
-## Rainfall data -----
+## b. Rainfall data -----
+# units = mm
 
 rain <- read.fwf("raw_data/daily_rainfall_NWEP_HadUKP.txt", 
                  widths = c(5, 5, rep(7, 31)),
@@ -104,14 +124,59 @@ names(rain) <- c("Year", "Month", seq(1, 31, by = 1))
 
 rain[rain==-99.99] <- NA
 
-## COVID data ----
+# wide to long 
 
-covid <- read.csv("raw_data/region_2022-02-07.csv")
+rain <- melt(rain, id = c("Year", "Month"))
 
-## Population data ----
+names(rain)[c(3,4)] <- c("Day", "rain")
+
+# order data by Year, Month and Day 
+
+rain <- rain[order(rain$Year,
+                   rain$Month,
+                   rain$Day),]
+
+# create date column
+
+rain$date <- paste(rain$Year, rain$Month, rain$Day,
+                   sep = "-")
+rain$date <- ymd(rain$date)
+
+## c. COVID data ----
+
+covid <- read.csv("raw_data/region_2022-02-24.csv")
+reinf <- read.csv("raw_data/reinfections.csv")
+
+covid$date <- ymd(covid$date)
+reinf$date <- ymd(reinf$date)
+
+covid <- merge(covid, reinf[,c(4,5)], by = "date")
+
+# merge covid data with environmental data 
+
+covid <- merge(covid, rain[,c(4,5)], by = "date")
+covid <- merge(covid, mean.temp[,c(4,5)], by = "date")
+covid <- merge(covid, min.temp[,c(4,5)], by = "date")
+covid <- merge(covid, max.temp[,c(4,5)], by = "date")
+
+## d. Population data ----
 # copied from "raw_data/ukpopestimatesmid2020on2021geography.xls"
   # sheet "MYE2 - Persons"
 
 # population estimate for North-West England for mid-2020
-pop <- 7367456
+covid$pop <- rep(7367456, length(covid$date))
 
+# 2.Save data as csv file ----
+
+write.csv(covid, file = "data/covid_data.csv", na = "NA",
+          row.names = FALSE)
+# test plots 
+
+plot(covid$date, log(covid$newCasesBySpecimenDate), type = "l")
+plot(covid$date, log(covid$newDeaths28DaysByDeathDate), type = "l")
+plot(covid$date, covid$newPeopleVaccinatedSecondDoseByVaccinationDate,
+     type = "l")
+plot(covid$date, covid$rain, type = "l")
+matplot(covid$date, cbind(covid$min.temp, covid$mean.temp, covid$max.temp),
+        type = "l", lty = c("dashed", "solid", "dashed"),
+        col = 1)
