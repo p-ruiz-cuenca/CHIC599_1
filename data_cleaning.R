27e17db5f5c7c3a01e21164d28effd5b4cde17af
admin <admin@hib005000016.lancs.local>
2022-03-18 14:41:47 +0100

    cleaned up covid data set and code to create it

--- data_cleaning.R
+++ data_cleaning.R
@@ -211,18 +211,79 @@ covid$newPeopleVaccinatedSecondDoseByVaccinationDate[is.na(covid$newPeopleVaccin
 covid$newPeopleVaccinatedThirdInjectionByVaccinationDate[is.na(covid$newPeopleVaccinatedThirdInjectionByVaccinationDate)] <- 0
 
 
-# merge covid data with environmental data 
+### merge covid data with environmental data ----
 
 covid <- merge(covid, rain[,c(4,5)], by = "date")
 covid <- merge(covid, mean.temp[,c(4,5)], by = "date")
 covid <- merge(covid, min.temp[,c(4,5)], by = "date")
 covid <- merge(covid, max.temp[,c(4,5)], by = "date")
 
+### create lagged variables ----
+
+# Create "t" variable 
+covid$t <- as.numeric(covid$date)-(min(as.numeric(covid$date)))+1
+
+# create function 
+
+create.lag.var <- function(x,lag,data) {
+  tlag <- data$t-lag
+  ind.lag <- sapply(tlag,function(k) {
+    out <- which((data$t-k)==0)
+    if(length(out)==0) out <- NA
+    return(out)
+  })
+  x[ind.lag]
+}
+
+# for() loop to create lagged variables
+
+env.vars <- c("rain", "mean.temp", "min.temp", "max.temp")
+
+for (i in 1:length(env.vars)) {
+  
+  lags <- c(2, 7, 10, 28)
+  
+  for (j in 1:length(lags)) {
+    
+    covid[[paste0(env.vars[i], ".lag.", lags[j])]] <- create.lag.var(x = covid[[env.vars[i]]],
+                                                                     lag = lags[j],
+                                                                     data = covid)
+    
+  }
+  
+}
+
+# Change NA's from lagged variables into actual data from environmental data sets
+  # Done by looking up dates where NA's are present
+  # finding the dates they correspond to
+  # and finding the corresponding environmental data for those dates
+
+covid$rain.lag.2[is.na(covid$rain.lag.2)] <- rain$rain[which(rain$date%in%(covid$date[is.na(covid$rain.lag.2)]-2))]
+covid$rain.lag.7[is.na(covid$rain.lag.7)] <- rain$rain[which(rain$date%in%(covid$date[is.na(covid$rain.lag.7)]-7))]
+covid$rain.lag.10[is.na(covid$rain.lag.10)] <- rain$rain[which(rain$date%in%(covid$date[is.na(covid$rain.lag.10)]-10))]
+covid$rain.lag.28[is.na(covid$rain.lag.28)] <- rain$rain[which(rain$date%in%(covid$date[is.na(covid$rain.lag.28)]-28))]
+
+covid$mean.temp.lag.2[is.na(covid$mean.temp.lag.2)] <- mean.temp$mean.temp[which(mean.temp$date%in%(covid$date[is.na(covid$mean.temp.lag.2)]-2))]
+covid$mean.temp.lag.7[is.na(covid$mean.temp.lag.7)] <- mean.temp$mean.temp[which(mean.temp$date%in%(covid$date[is.na(covid$mean.temp.lag.7)]-7))]
+covid$mean.temp.lag.10[is.na(covid$mean.temp.lag.10)] <- mean.temp$mean.temp[which(mean.temp$date%in%(covid$date[is.na(covid$mean.temp.lag.10)]-10))]
+covid$mean.temp.lag.28[is.na(covid$mean.temp.lag.28)] <- mean.temp$mean.temp[which(mean.temp$date%in%(covid$date[is.na(covid$mean.temp.lag.28)]-28))]
+
+covid$min.temp.lag.2[is.na(covid$min.temp.lag.2)] <- min.temp$min.temp[which(min.temp$date%in%(covid$date[is.na(covid$min.temp.lag.2)]-2))]
+covid$min.temp.lag.7[is.na(covid$min.temp.lag.7)] <- min.temp$min.temp[which(min.temp$date%in%(covid$date[is.na(covid$min.temp.lag.7)]-7))]
+covid$min.temp.lag.10[is.na(covid$min.temp.lag.10)] <- min.temp$min.temp[which(min.temp$date%in%(covid$date[is.na(covid$min.temp.lag.10)]-10))]
+covid$min.temp.lag.28[is.na(covid$min.temp.lag.28)] <- min.temp$min.temp[which(min.temp$date%in%(covid$date[is.na(covid$min.temp.lag.28)]-28))]
+
+covid$max.temp.lag.2[is.na(covid$max.temp.lag.2)] <- max.temp$max.temp[which(max.temp$date%in%(covid$date[is.na(covid$max.temp.lag.2)]-2))]
+covid$max.temp.lag.7[is.na(covid$max.temp.lag.7)] <- max.temp$max.temp[which(max.temp$date%in%(covid$date[is.na(covid$max.temp.lag.7)]-7))]
+covid$max.temp.lag.10[is.na(covid$max.temp.lag.10)] <- max.temp$max.temp[which(max.temp$date%in%(covid$date[is.na(covid$max.temp.lag.10)]-10))]
+covid$max.temp.lag.28[is.na(covid$max.temp.lag.28)] <- max.temp$max.temp[which(max.temp$date%in%(covid$date[is.na(covid$max.temp.lag.28)]-28))]
+
+
 ## d. Population data ----
 # copied from "raw_data/ukpopestimatesmid2020on2021geography.xls"
   # sheet "MYE2 - Persons"
 
-# population estimate for North-West England for mid-2020
+# population estimate for North-West England for mid-2020 = 7,367,456
 covid$pop <- rep(7367456, length(covid$date))
 
 # 2.Save data as csv file ----
@@ -232,21 +293,30 @@ covid$pop <- rep(7367456, length(covid$date))
 names(covid)[5:10] <- c("cases", "deaths.28", "deaths.60", "vacc.2nd", "vacc.3rd",
                         "reinf")
 
+# re-arrange columns 
+
+names(covid)[c(1,15,2:4, 32, 5:11, 16:19, 12, 20:23, 13, 24:27, 14, 28:31)]
+
+covid <- covid[,c(1,15,2:4, 32, 5:11, 16:19, 12, 20:23, 13, 24:27, 14, 28:31)]
+
 # Include verbose labels 
-library(expss)
-
-covid <- apply_labels(covid,
-             cases = "New cases by specimen date",
-             deaths.28 = "New deaths 28 days after diagnosis, by death date",
-             deaths.60 = "New deaths 60 days after diagnosis, by death date",
-             reinf = "New reinfections by specimen date")
-
-names(covid)
-var_lab(covid$deaths.28)
+# doesn't really work well
+#library(expss)
+#
+#covid <- apply_labels(covid,
+#             cases = "New cases by specimen date",
+#             deaths.28 = "New deaths 28 days after diagnosis, by death date",
+#             deaths.60 = "New deaths 60 days after diagnosis, by death date",
+#             reinf = "New reinfections by specimen date")
+#
+#names(covid)
+#var_lab(covid$deaths.28)
+#
 
 # save as csv 
 write.csv(covid, file = "data/covid_data.csv", na = "NA",
           row.names = FALSE)
+
 # test plots 
 
 plot(covid$date, log(covid$newCasesBySpecimenDate), type = "l")
